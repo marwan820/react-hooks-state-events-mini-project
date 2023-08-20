ea55dfb188e1f0607cffacc3725a24d580e38550
Marwan Rahim <marwan820@Marwan>
2023-08-20 16:35:12 -0500

    passed props and created states

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+  const [selectedCategory,setselectedCategory]= useState("All")
+function onCategoryClicked(category){
+  setselectedCategory(category)
+
+}
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+    {categories.map(category => <button  className={`${selectedCategory === category ? `selected`: ``}`} onClick={onCategoryClicked(category)}key={category}>{category}</button>)
+    }
     </div>
   );
 }
